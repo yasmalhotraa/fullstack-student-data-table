a67ef01cce6fb9e0a97976f7f83e396317e084d8
yasmalhotraa <yashmalhotra330@gmail.com>
2024-12-24 03:54:34 +0530

    chapterherostyling

--- src/SidebarOptions/Chapter.jsx
+++ src/SidebarOptions/Chapter.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Chapter() {
-  return <div>Chapter</div>;
+  return (
+    <div className="flex justify-center items-center w-full h-full text-5xl text-gray-400">
+      <span className="border-dashed border-4 p-4">Chapter</span>
+    </div>
+  );
 }
 
 export default Chapter;
