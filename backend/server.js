d334710937887375ed8412c3c11143d12455126e
yasmalhotraa <yashmalhotra330@gmail.com>
2024-12-24 02:02:01 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -134,7 +134,7 @@ app.delete("/api/students/:id", async (req, res) => {
 });
 
 // Start the server on a specified port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
